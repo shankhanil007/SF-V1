af93568e910d382a2e851b868130062f942d81e7
shankhanil007 <shankhanilborthakur@gmail.com>
2021-08-15 15:26:15 +0530

    MEN Project setup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 //dependencies and setup
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./models/user");
 const app = express();
 const path = require("path");
 
@@ -7,18 +14,79 @@ require("dotenv").config();
 
 const router = (global.router = express.Router());
 const port = process.env.PORT || 5000;
-var cors = require("cors");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public"));
-//^idk why there are two. I am not touching this because some path somewhere will break.
+app.use(express.static("public"));
+
+//------------- Initialising passport ----------------
+
+app.use(
+  require("express-session")({
+    secret: "This the secret message for authentication",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//------------------------   Authentication Routes  -------------------------
+
+app.get("/login", function (req, res) {
+  res.render("login.ejs");
+});
+app.get("/signup", function (req, res) {
+  res.render("signup.ejs");
+});
+
+app.post("/signup", function (req, res) {
+  User.register(
+    new User({
+      username: req.body.username,
+      name: req.body.name,
+      phone: req.body.phone,
+    }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
+    }
+  );
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+);
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+// ------------------------ Authentication Ends ------------------------------
 
 //backend routes
-app.use("/api/user", require("./routes/user.js"));
 app.use("/api/room", require("./routes/room.js"));
 app.use(router);
 
@@ -38,7 +106,7 @@ app.get("/profile/:user_id", (req, res) => {
 });
 
 //socket.io
-var server = require("http").Server(app);
+const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
